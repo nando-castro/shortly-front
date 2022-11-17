fa390321a7438f17bcd9d1a68a5eb857e4aab1f1
nando-castro <fernando.castro10.master@gmail.com>
2022-11-16 21:10:18 -0300

    feat: create service api

---
+++ src/service/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+// //Deploy
+// export const api = axios.create({
+//   baseURL: "Deploy",
+// });
+
+//Locally
+export const api = axios.create({
+  baseURL: "http://localhost:5000",
+});
